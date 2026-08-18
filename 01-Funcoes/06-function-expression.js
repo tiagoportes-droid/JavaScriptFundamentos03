ee88a7bbf66c7b8fa062fd886b36ec54dcba9e3c
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-08-18 11:32:30 -0300

    feat: Adicionando informações a 06-funciton-expression.js

---
+++ 01-Funcoes/06-function-expression.js
@@ -0,0 +1,10 @@
+function somarTradicional(a, b){
+    return a + b;
+}
+
+const somarAnonimo = function (a,b) {
+    return a + b;
+}
+
+console.log(somarTradicional(10, 5));
+console.log(somarAnonimo(10, 5));
