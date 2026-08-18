20bd76d4bf2d28b4d668dc5b009e6e7128fa02a2
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-08-18 13:57:40 -0300

    feat: Adincinado madulos ao 03-filter.js

---
+++ 02-metodos-array/02-map.js
@@ -0,0 +1,12 @@
+const numeros = [1, 2, 3, 4, 5];
+
+const dobrado = numeros.map((num) => num * 2);
+
+console.log(dobrado);
+
+
+let nomes = ["Brenda", "Carlos", "Diana", "Felipe"];
+
+let mensagem = nomes.map((nomes) => `Olá, ${nomes.toUpperCase()}`);
+
+console.log(mensagem);
