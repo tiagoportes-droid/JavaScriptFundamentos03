c25db7a7709413b0b81a3244ea2cab390c58b074
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-08-18 09:37:14 -0300

    feat: Adicioando informaçoes a 03-retunr.js e 04-media.js

--- 01-Funcoes/02-parametros.js
+++ 01-Funcoes/02-parametros.js
@@ -2,12 +2,17 @@ function apresentar(nome) {
   console.log(`Olá, ${nome}`);
 }
 
-
 apresentar("Tiago");
 
-
-function somar(a, b){
-    console.log(`${a} + ${b} = ${a+b}`);
+function somar(a, b) {
+  console.log(`${a} + ${b} = ${a + b}`);
 }
 
-somar(10, 20)
+somar(10, 20);
+
+function apresentar(nome = "Visitante"){
+    console.log(`Ola, ${nome}`);
+}
+
+apresentar();
+apresentar("Tiago");
