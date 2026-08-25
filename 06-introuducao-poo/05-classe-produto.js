e2dfd4cb3435a891dfe106c15bb3e5ec3960bbbe
Tiago Portes <tiago.fportes@gmail.com>
2026-08-25 09:44:15 -0300

    feat: adiconando infromaçoes a 06-introducao-poo

---
+++ 06-introuducao-poo/05-classe-produto.js
@@ -0,0 +1,18 @@
+class Produto {
+  constructor(produto, preco) {
+    this.produto = produto;
+    this.preco = preco;
+  }
+
+  aplicarDesconto(percentual) {
+    this.preco -= this.preco * (percentual / 100);
+  }
+}
+
+const produto = new Produto("Memoria RAM", 400);
+console.log(produto);
+produto.aplicarDesconto(10);
+console.log(produto);
+
+const produto1 = new Produto("Mouse", 100);
+console.log(produto1);
