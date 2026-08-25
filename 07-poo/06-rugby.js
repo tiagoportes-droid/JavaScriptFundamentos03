9dd788b9672c8990f5c27bb6c86a372cfd40be34
Tiago Portes <tiago.fportes@gmail.com>
2026-08-25 10:54:26 -0300

    feat: add 06-rugby

--- 07-poo/06-rugby.js
+++ 07-poo/06-rugby.js
@@ -5,17 +5,29 @@ class JogadorRugby {
     this.posicao = posicao;
   }
 
-  correr(){
-    console.log(`${this.nome} esta correndo com a bola`)
+  correr() {
+    console.log(`${this.nome} esta correndo com a bola`);
   }
 
-  passarBola(){
-    console.log(`${this.nome} passou a bola`)
+  passarBola() {
+    console.log(`${this.nome} passou a bola`);
   }
 
-  mostraDados(){
-    console.log(`Nome ${this.nome}`)
-    console.log(`Número ${this.numero}`)
-    console.log(`Posição ${this.posicao}`)
+  mostraDados() {
+    console.log(`Nome ${this.nome}`);
+    console.log(`Número ${this.numero}`);
+    console.log(`Posição ${this.posicao}`);
   }
 }
+
+const jogador1 = new JogadorRugby("Pablo", 10, "Abertura");
+
+jogador1.mostraDados();
+jogador1.correr();
+jogador1.passarBola();
+
+const jogador2 = new JogadorRugby("Tardin", 11, "Hulker");
+
+jogador2.mostraDados();
+jogador2.correr();
+jogador2.passarBola();
